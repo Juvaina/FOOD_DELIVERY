0bc8350f4ca8fde805dc88b162f1c5e0ea97b5ab
Juvaina <juvaina_mammikkakath_from.isg@hdb.gov.sg>
2025-05-16 10:30:08 +0800

    Updated mongo uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-  await mongoose
-    .connect(
-      'mongodb+srv://juni160589:Junibavu1342@cluster0.sobwh.mongodb.net/food-del'
-    )
-    .then(() => console.log('DB Connected'));
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('DB Connected');
+  } catch (error) {
+    console.error('DB connection error:', error);
+    throw error;
+  }
 };
